640638248d797c1edffb2e990dc1d361e31f08c6
lukasz.filiciak <lukasz.filiciak@farmaprom.pl>
2024-05-16 07:46:41 +0200

    Initial Commit

---
+++ GPTtest/GPTtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTtestApp.swift
+//  GPTtest
+//
+//  Created by Łukasz Filiciak on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GPTtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
